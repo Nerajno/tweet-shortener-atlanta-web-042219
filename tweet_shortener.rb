4eac656cbc4676d48d8ba74662a13896d4093a55
active-object-6703 <nerajno@gmail.com>
2019-04-03 01:04:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,8 +2,11 @@
 def word_substituter(tweet)
   dictionary =
     {"hello" => 'hi',
-      "to"||"two"||"too" => '2',
-      "for"||"four" => '4',
+      "to"=> '2',
+      "two"=> '2',
+      "too" => '2',
+      "for"=>'4',
+      "four" => '4',
       'be' => 'b',
       'you' => 'u',
       "at" => "@",
